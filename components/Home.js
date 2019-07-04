4dc3dade70b3fa74961b94f12c61b9eaca6bf3e0
Oscar Bernal <oscarbernal93@gmail.com>
2019-07-04 15:33:53 +0200

    improvements on the Carousel

--- components/Home.js
+++ components/Home.js
@@ -24,6 +24,7 @@ export default class Home extends Component {
         itemWidth={iw}
         style={{width}}
         data={data}
+        useVelocityForIndex={false}
         contentOffset={co}
         onIndexChange={index =>
           this.setState(() => ({ currentIndex: index }))
