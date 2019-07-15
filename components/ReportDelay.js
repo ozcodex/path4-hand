3f6acf2fe10c757b1eaa1e36ed6819dc727a959f
Oscar Bernal <oscarbernal93@gmail.com>
2019-07-15 17:14:45 +0200

    saving delay information on the db

--- components/ReportDelay.js
+++ components/ReportDelay.js
@@ -1,10 +1,11 @@
 import React,{ Component } from 'react';
-import { View, Text, Dimensions, TouchableOpacity, Picker } from 'react-native';
+import { View, Text, Dimensions, TouchableOpacity, Picker, Alert } from 'react-native';
 import Image from 'react-native-scalable-image';
 import CircleSlider from './CircleSlider';
 import Nav from './Nav';
 import {border, styles} from '../styles'
 import { YellowBox } from 'react-native';
+import Constants from 'expo-constants';
 const firebase = require('../firebase.js');
 const db = firebase.db;
 const files = firebase.files;
@@ -20,25 +21,23 @@ static navigationOptions = { header: null }
     const line = navigation.getParam('line')
     const station = navigation.getParam('station')
     const direction = navigation.getParam('direction')
-    if (!line || !station) {
+    if (!line || !station || !direction) {
       //if there are no line redirect to the begining
       navigation.navigate('ReportStation')
-    }else{
-      db.collection('lines').where('name', '==', line).get().then(
-        snapshot => {
-          var directions = snapshot.docs[0].data().directions
-          console.log(line, directions)
-          this.setState({
-            directions: directions,
-            direction: directions[0]
-          })
-        }
-      );
     }
+    let ranges= [
+      'No delay',          //0
+      '1 - 5 minutes',     //1
+      '6 - 10 minutes',    //2
+      '11 - 15 minutes',   //3
+      '16 or more minutes' //4
+    ]
     this.state = {
       line: line,
       station: station,
-      direction: 'false',
+      direction: direction,
+      delay: '0',
+      ranges: ranges
     };
     YellowBox.ignoreWarnings(['Setting a timer']);
     //Ignoring it is not the best approach, but if you're using Firebase Realtime Database.
@@ -48,23 +47,40 @@ static navigationOptions = { header: null }
 
   render() {
     const navigate = this.props.navigation.navigate;
+    const saveAndContinue = () => {
+      let uuid = Constants.deviceId;
+      let data = {
+        user: uuid,
+        tiemstamp: Date.now(),
+        line: this.state.line,
+        direction: this.state.direction,
+        station: this.state.station,
+        delay: this.state.delay
+        }
+      console.log('data2save: ',data)
+      let ref = db.collection('delays').doc();
+      ref.set(data).then(()=> {
+        Alert.alert('Information stored successfully')
+        navigate('Home')
+      }).catch(error => Alert.alert('error saving the info :('))
+    }
     return (
       <View style={styles.container}>
-        <Text style={styles.text}>In which diretion are you driving?</Text>
+        <Text style={styles.text}>How much delay did you had?</Text>
         <View style={{borderColor: 'gray', borderWidth: 1}}>
           <Picker
-            selectedValue={this.state.direction}
+            selectedValue={this.state.delay}
             style={styles.picker}
             mode='dropdown'
             onValueChange={(itemValue) =>
-              this.setState({direction: itemValue})
+              this.setState({delay: itemValue})
             }>
-            {this.state.directions? this.state.directions.map((item) => (
-              <Picker.Item label={item} key={item} value={item} />
-            )) : <Picker.Item label='Loading...' key='undef' value='false' />}
+            {this.state.ranges.map((item,key) => (
+              <Picker.Item label={item} key={key} value={key} />
+            ))}
           </Picker>
         </View>
-        <TouchableOpacity style={styles.button} onPress={()=>{navigate('ReportDelay',{line: this.state.line,direction: this.state.direction,station: this.state.station})}}>
+        <TouchableOpacity style={styles.button} onPress={saveAndContinue}>
           <Text style={styles.buttonText}>Next</Text>
         </TouchableOpacity>
       </View>
